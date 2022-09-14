815224421a0a3524a228a2aacc2f2e42b6cbafcc
pupil <pupil@ivkhk.ee>
2022-09-14 15:16:41 +0300

    Одномерный и двухмерные массивы

--- src/jptv21arrays/JPTV21Arrays.java
+++ src/jptv21arrays/JPTV21Arrays.java
@@ -5,6 +5,8 @@
  */
 package jptv21arrays;
 
+import java.util.Random;
+
 /**
  *
  * @author pupil
@@ -15,7 +17,55 @@ public class JPTV21Arrays {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        Random random = new Random();
+        int n = 10;
+        int sum = 0;
+        int min = 10;
+        int max = 0;
+        System.out.println("---- Одномерные массивы ----");
+        int[] nums = new int[n];
+        for (int i = 0; i < nums.length; i++){
+             nums[i] = random.nextInt(n);
+        }
+        for (int i = 0; i < nums.length; i++){
+             System.out.printf("%3d",nums[i]);
+             sum += nums[i];
+             if(min > nums[i]) min = nums[i];
+             if(max < nums[i]) max = nums[i];
+        }
+        System.out.println();
+        System.out.println("sum = " +sum);
+        System.out.println("min = " +min);
+        System.out.println("max = " +max);
+        }
+        
+//        System.out.println("---- Многомерные массивы ----");
+//        int[][] nums2 = new int [n][n];
+//        for(int i = 0; i < nums2.length; i++) {
+//            for(int j = 0: j < nums2[i].length; j++);
+//                    nums2[i][j] = random.nextInt(n);
+//            }
+//        }
+//        sum = 0;
+//        for(int i = 0; i < num2.length; i++) {
+//            for(int j = 0; j < nums2[i].length; j++) {
+//                System.out.printf("%4d", nums2[i] [j]);
+//                sum += nums2 [i] [j];
+//            }
+//            System.out.println();
+//        
+//            
+//        }
+//        System.out.println();
+//        System.out.println("sum = " +sum);
+//        System.out.println("min = " +min);
+//        System.out.println("max = " +max);
+//        System.out.println("Сумма по нисходящей диагонали = " +SumDown);
+//        System.out.println("Сумма по изходящей диагонали = " +SumUp);
+
+            
+        
+       
     
-}
+    
+
